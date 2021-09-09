7c1f1bddecaaba97ce7a7e588a3d8600972b2094
milesfried <mafriedman@smu.edu>
2021-09-09 16:47:46 -0500

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,187 @@
 #include <iostream>
+#include <fstream>
+#include "wall.h"
+#include <vector>
+#include <algorithm>
+//Heap's Algorithm Reference Used: https://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
 
-int main() {
-    std::cout << "Fall 2021 CS 3353 PA 01." << std::endl;
+using namespace std;
+void bruteForceAlgo(vector<painting>& paintings, wall canvas, string fileName);
+void expenseAlgo(vector<painting>& paintings, wall canvas, string fileName);
+void customAlgo(vector<painting>& paintings, wall canvas, string fileName);
+void writeOut(ofstream& fileOut, wall& canvas);
+void paintingsAdd(vector<painting> paintings, wall& canvas);
+void heapPermutation(vector<painting>& paintings, int size, int n, wall canvas);
+
+wall bruteMax;
+int main(int argC, char* argV[]) {
+    vector<painting> paintings;
+    int numPaintings;
+    wall canvas;
+
+    if (argC == 2) {
+        ifstream file;
+        file.open(argV[1]);
+        if (!file.is_open()) {
+            cout << "Could not open file" << endl;
+        } else {
+            int w;
+            int h;
+            file >> w;
+            file >> h;
+            wall canvas(w, h);
+
+            file >> numPaintings;
+            int id;
+            int value;
+            for (int i = 0; i < numPaintings; i++) {
+                file >> id;
+                file >> value;
+                file >> w;
+                file >> h;
+                painting temp(id, value, w, h);
+                paintings.push_back(temp);
+            }
+            bruteForceAlgo(paintings, canvas, argV[1]);
+            expenseAlgo(paintings, canvas, argV[1]);
+            customAlgo(paintings, canvas, argV[1]);
+
+        }
+    } else {
+        cout << "Improper Command Line Arguments Only Input 1 File Name. Example:"
+             << endl <<
+             "/Users/yeet/Downloads/21f-pa01-thelads-main/cmake-build-debug/21f_pa01_thelads_main input.txt"
+             << endl;
+    }
+//wall ohboy(1000, 1000);
+//
+//painting ayo7(1, 100, 100, 100);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//ohboy.addPainting(ayo7);
+//    ohboy.addPainting(ayo7);
+//    ohboy.addPainting(ayo7);
+//    for(int i = 0; i < ohboy.getList().size(); i++){
+//    cout << "Painting " << i + 1 << " X: " << ohboy.getList()[i].getX() << endl;
+//    cout << "Painting " << i + 1 << " Y: " << ohboy.getList()[i].getY() << endl;
+//}
+
+
+
+
+
 }
+void bruteForceAlgo(vector<painting>& paintings, wall canvas, string fileName){
+    size_t found = fileName.find_last_of('.');
+    string bruteName = fileName.insert(found, "-bruteforce");
+    ofstream fileOut(bruteName.c_str());
+
+    if (paintings.size() <= 10){
+        heapPermutation(paintings, paintings.size(), paintings.size(),canvas);
+        writeOut(fileOut, bruteMax);
+        fileOut.close();
+
+    }
+    else{
+        fileOut << "data set exceeds maximum";
+    }
+
+
+}
+void expenseAlgo(vector<painting>& paintings, wall canvas, string fileName){
+    size_t found = fileName.find_last_of('.');
+    string expenseName = fileName.insert(found, "-highvalue");
+    ofstream fileOut(expenseName.c_str());
+
+    for(int i = 0; i < paintings.size() - 1; ++i){
+        int indexBiggest = i;
+        for (int j = i + 1; j < paintings.size(); ++j){
+            if(paintings.at(j) > paintings.at(indexBiggest)){
+                indexBiggest = j;
+            }
+        }
+        painting temp = paintings.at(i);
+        paintings.at(i) = paintings.at(indexBiggest);
+        paintings.at(indexBiggest) = temp;
+    }
+    for(auto & painting : paintings){
+        canvas.addPainting(painting);
+    }
+    writeOut(fileOut, canvas);
+    fileOut.close();
+
+}
+
+void customAlgo(vector<painting>& paintings, wall canvas, string fileName){
+    size_t found = fileName.find_last_of('.');
+    string customName = fileName.insert(found, "-custom");
+    ofstream fileOut(customName.c_str());
+    for(int i = 0; i < paintings.size() - 1; ++i){
+        int indexSmallest = i;
+        for (int j = i + 1; j < paintings.size(); ++j){
+            if(paintings.at(j) < paintings.at(indexSmallest)){
+                indexSmallest = j;
+            }
+        }
+        painting temp = paintings.at(i);
+        paintings.at(i) = paintings.at(indexSmallest);
+        paintings.at(indexSmallest) = temp;
+    }
+    for(auto & painting : paintings){
+       // cout << painting.getID() << endl;
+        canvas.addPainting(painting);
+    }
+
+    writeOut(fileOut, canvas);
+    fileOut.close();
+
+}
+
+void heapPermutation(vector<painting>& paintings, int size, int n, wall canvas){
+    if (size == 1){
+        paintingsAdd(paintings, canvas);
+        if(canvas.getValue() >= bruteMax.getValue()){
+            bruteMax = canvas;
+        }
+    }
+
+    for (int i = 0; i < size; i++){
+        heapPermutation(paintings, size - 1, n, canvas);
+        if (size % 2 == 1){
+            painting temp = paintings[0];
+            paintings[0] = paintings[size -1];
+            paintings[size - 1] = temp;
+        }
+        else{
+            painting temp = paintings[i];
+            paintings[i] = paintings[size -1];
+            paintings[size - 1] = temp;
+        }
+    }
+
+}
+void paintingsAdd(vector<painting> paintings, wall& canvas){
+    canvas.clear();
+    for (int i = 0; i < paintings.size(); i++){
+        canvas.addPainting(paintings[i]);
+    }
+}
+void writeOut(ofstream& fileOut, wall& canvas){
+    //fileOut << "ID " << "Value " << "Width " << "Height " << "X " << "Y " << endl;
+    for(int i = 0; i < canvas.getList().size(); i++){
+        fileOut << canvas.getList().at(i).getID() << " ";
+        fileOut << canvas.getList().at(i).getValue() << " ";
+        fileOut << canvas.getList().at(i).getWidth() << " ";
+        fileOut << canvas.getList().at(i).getHeight() << " ";
+        fileOut << canvas.getList().at(i).getX()<< " ";
+        fileOut << canvas.getList().at(i).getY() << endl;
+
+    }
+    fileOut << canvas.getValue() << endl;
+}
+
---
+++ src/wall.cpp
@@ -0,0 +1,126 @@
+//
+// Created by Miles Friedman on 9/2/21.
+//
+
+#include "wall.h"
+
+wall::wall(int w, int h) {
+    width = w;
+    height = h;
+
+
+}
+
+void wall::addPainting(painting &newPainting) {
+    if (newPainting.getWidth() > width || newPainting.getHeight() > height) {
+    }
+    else if (wallList.size() == 0) {
+        newPainting.setX(0);
+        newPainting.setY(0);
+        cout << "Adding " << newPainting.getID() << endl;
+        wallList.push_back(newPainting);
+
+    } else {
+        if (max_X + newPainting.getWidth() < width) {
+
+            newPainting.setX(max_X+ newPainting.getWidth() + 1);
+        } else {
+            newPainting.setX(0);
+            max_X = 0;
+            if (max_Y + newPainting.getHeight() < height) {
+                newPainting.setY(max_Y + newPainting.getHeight() + 1);
+            } else {
+                newPainting.setY(0);
+            }
+        }
+
+
+//            newPainting.setX(0);
+//            newPainting.setY(0);
+//            for (int i = 0; i < wallList.size(); i++) {
+//
+//                while (collides(newPainting, wallList[i])) {
+//                    if (newPainting.getX() + newPainting.getWidth() > width) {
+//                        newPainting.setX(0);
+//                        newPainting.setY(wallList[i].getY() + wallList[i].getHeight() + 1);
+//
+//                    } else {
+//                        newPainting.setY(0);
+//                        newPainting.setX(wallList[i].getX() + wallList[i].getWidth() + 1);
+//                    }
+//                }
+
+
+
+
+        int l = 0;
+
+        for (int i = 0; i < wallList.size(); i++) {
+            if (!collides(newPainting, wallList[i])) {
+                l++;
+            }
+        }
+        if ((newPainting.getX() + newPainting.getWidth()) > width) {
+            l = 0;
+        }
+        if ((newPainting.getY() + newPainting.getHeight()) > height){
+            l = 0;
+        }
+        if (l >= wallList.size()) {
+            if (newPainting.getX() + newPainting.getWidth() > max_X) {
+                max_X = newPainting.getX() + newPainting.getWidth();
+
+            }
+            if (newPainting.getY() + newPainting.getHeight() > max_Y) {
+                max_Y = newPainting.getY() + newPainting.getHeight();
+
+            }
+            cout << "Adding " << newPainting.getID() << endl;
+            wallList.push_back(newPainting);
+        }
+    }
+    updateValue();
+}
+
+
+bool wall::collides(painting &paint1, painting &paint2) {
+    if(paint1.getX() > paint2.getX() + paint2.getWidth()
+       || paint2.getX() > paint1.getX() + paint2.getWidth()){
+        return false;
+    }
+    if (paint1.getY() > paint2.getY() + paint2.getHeight() ||
+        paint2.getY() > paint1.getY() + paint1.getHeight()){
+        return false;
+    }
+    return true;
+}
+
+void wall::wallPrint() {
+    for(int i = 0; i < getList().size(); i++){
+        cout << "Painting " << i + 1 << " X: " << getList()[i].getX() << endl;
+        cout << "Painting " << i + 1 << " Y: " << getList()[i].getY() << endl;
+
+    }
+
+}
+
+wall::wall() {
+    width = 0;
+    height = 0;
+}
+
+int wall::getValue() {
+    return totalValue;
+}
+
+void wall::clear() {
+    wallList.clear();
+}
+
+void wall::updateValue() {
+    totalValue = 0;
+    for(auto & i : wallList){
+        totalValue += i.getValue();
+    }
+}
+
